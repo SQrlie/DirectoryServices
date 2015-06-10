87c2314c80986eea004b057cdceec3217cef13d2
teggi <juicetyven@gmail.com>
2015-06-10 14:02:15 +0200

    Laziness

---
+++ repl.ps1
@@ -0,0 +1,30 @@
+﻿function New-ReplicationLink {
+    param(
+        [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName)]
+        [string]$SourceDC,
+        [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName)]
+        [string]$DestinationDC,
+        [Parameter(Mandatory=$False,ValueFromPipelineByPropertyName)]
+        [string]$Server,
+        [Parameter(Mandatory=$False)]
+        [Management.Automation.PSCredential]$Credential,
+        [Parameter(Mandatory=$False)]
+        [switch]$WhatIf
+    )
+$DestinationDCShortName = $DestinationDC.Split(".")[0].ToUpper()
+$ObjectPath = (Get-DSDomainController $SourceDC).GetDirectoryEntry().Children.DistinguishedName
+$OtherAttributes = @{
+    FromServer = (Get-DSDomainController $DestinationDC).GetDirectoryEntry().Children.DistinguishedName
+    Options = "0x0"
+    EnabledConnection = "True"
+}
+
+if ($WhatIf) {
+    New-DSObject -Name "IP-FROM-$($DestinationDCShortName)" -Path $ObjectPath -Credential $Credential -OtherAttributes $OtherAttributes -Type "nTDSConnection" -WhatIf
+} else {
+    try {
+        New-DSObject -Name "IP-FROM-$($DestinationDCShortName)" -Path $ObjectPath -Credential $Credential -OtherAttributes $OtherAttributes -Type "nTDSConnection" -Verbose
+    } catch {
+    }
+}
+}
